d07ebe22873618f342efebc6627cd68406491b77
kritika2812 <62373184+kritika2812@users.noreply.github.com>
2020-07-13 11:32:59 +0530

    Update mail_php.py

--- mail_php.py
+++ mail_php.py
@@ -2,9 +2,9 @@ import smtplib, ssl
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
-sender_email = "shahkritika99@gmail.com"  # Enter your address
-receiver_email = "shahkritika99@gmail.com"  # Enter receiver address
-password = input("Type your password and press enter:kritika1999! ")
+sender_email = ""  # Enter your address
+receiver_email = ""  # Enter receiver address
+password = input("Type your password and press enter: ")
 message = """\
 Subject: "PHP Site Is Running!"
 Project is done."""
